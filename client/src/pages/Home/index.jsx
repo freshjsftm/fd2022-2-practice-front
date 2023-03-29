867a21af79c9638ed85368866cca0098cfda1349
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-03-29 16:12:18 +0300

    Add component Advantage

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -8,6 +8,7 @@ import Footer from '../../components/Footer/Footer';
 import styles from './Home.module.sass';
 import carouselConstants from '../../carouselConstants';
 import Spinner from '../../components/Spinner/Spinner';
+import Advantage from '../../components/Advantage';
 
 const Home = props => {
   const [index, setIndex] = useState(0);
@@ -61,52 +62,7 @@ const Home = props => {
                 carouselType={carouselConstants.MAIN_SLIDER}
               />
             </div>
-            <div className={styles.container__description}>
-              <h2 className={styles.blueUnderline}>Why Squadhelp?</h2>
-              <div className={styles.cardContainer}>
-                <div className={styles.card}>
-                  <img
-                    src={`${CONSTANTS.STATIC_IMAGES_PATH}more-benifits-world-icon.png`}
-                    alt='globe'
-                  />
-                  <h3>Largest Naming Community</h3>
-                  <p>
-                    Our unique approach allows you to receive an unmatched
-                    breadth of business name ideas from world's largest
-                    community of naming experts. With 75,000+ creatives and
-                    15,000+ successful naming projects, Squadhelp is by far the
-                    largest naming platform across the globe .
-                  </p>
-                </div>
-                <div className={styles.card}>
-                  <img
-                    src={`${CONSTANTS.STATIC_IMAGES_PATH}more-benifits-high-quality-icon.png`}
-                    alt='desktop'
-                  />
-                  <h3>High Quality & Collaboration</h3>
-                  <p>
-                    Using an advanced Quality Scoring Algorithm and Machine
-                    Learning, we ensure that you receive more ideas from our
-                    top-quality creatives, and Gamification best practices
-                    ensure two-way communication throughout your contest.
-                  </p>
-                </div>
-                <div className={styles.card}>
-                  <img
-                    src={`${CONSTANTS.STATIC_IMAGES_PATH}more-benifits-trademark-icon.png`}
-                    alt='cards'
-                  />
-                  <h3>Agency-Level Features</h3>
-                  <p>
-                    Squadhelp's high end Audience Testing service allows you to
-                    poll your target demographics to get unbiased feedback on
-                    your favorite names. Also receive Trademark support from our
-                    team of Licensed Trademark Attorneys, so you can pick your
-                    name with confidence.
-                  </p>
-                </div>
-              </div>
-            </div>
+            <Advantage />
             <div className={styles.greyContainer}>
               <div className={styles.adv}>
                 <div className={styles.images}>
